e38c448dd2ac60528e2c155637bc51f43966a3b6
mortimr <iulian@rotaru.fr>
2022-12-15 12:13:59 +0100

    feat: add vTreasury entity and relations

--- src/NexusV1.mapping.ts
+++ src/NexusV1.mapping.ts
@@ -4,23 +4,30 @@ import {
 import {
   vFactory as vFactoryTemplate,
 } from "../generated/templates"
-import { vFactory } from "../generated/schema"
+import { vFactory, vTreasury } from "../generated/schema"
 import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleSpawnedFactory(event: SpawnedFactory): void {
-  const entity = new vFactory(event.params.factory)
-  entity.version = BigInt.fromI32(1)
-  entity.address = event.params.factory
-  
-  entity.operatorName = ""
-  entity.operatorUrl = ""
-  entity.operatorIconUrl = ""
-  entity.createdAt = event.block.timestamp
-  entity.editedAt = event.block.timestamp
-  entity.createdAtBlock = event.block.number
-  entity.editedAtBlock = event.block.number
+  const treasury = new vTreasury(event.params.treasury)
+  treasury.address = event.params.treasury
+  treasury.factory = event.params.factory
 
-  entity.save()
+  treasury.save()
+
+  const factory = new vFactory(event.params.factory)
+  factory.version = BigInt.fromI32(1)
+  factory.address = event.params.factory
+  factory.treasury = event.params.treasury
+  factory.operatorName = ""
+  factory.operatorUrl = ""
+  factory.operatorIconUrl = ""
+  factory.createdAt = event.block.timestamp
+  factory.editedAt = event.block.timestamp
+  factory.createdAtBlock = event.block.number
+  factory.editedAtBlock = event.block.number
+
+  factory.save()
+  
   vFactoryTemplate.create(event.params.factory);
 }
 
