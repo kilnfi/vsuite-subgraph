49dcb0cf8fe96a6a098c399b771db8b816c36136
Gauthier <legrangm@gmail.com>
2023-03-16 16:34:29 +0100

    feat: add commission

--- src/vNFTV1.mapping.ts
+++ src/vNFTV1.mapping.ts
@@ -1,6 +1,6 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 import { vNFT, vNFTIntegration, vNFTTransfer } from "../generated/schema";
-import { PurchasedValidator, SetExtraData, SetName, SetSymbol, SetURIPrefix, Transfer } from "../generated/vNFT/vNFTV1";
+import { PurchasedValidator, SetExtraData, SetIntegrator, SetIntegratorCommission, SetName, SetOperatorCommission, SetSymbol, SetURIPrefix, Transfer } from "../generated/vNFT/vNFTV1";
 import { uid } from "./utils";
 
 export function handleSetName(event: SetName): void {
@@ -15,6 +15,9 @@ export function handleSetName(event: SetName): void {
     e.createdAtBlock = event.block.number;
     e.supply = BigInt.fromI32(0);
     e.supply = BigInt.fromI32(0);
+    e.operatorCommission = BigInt.fromI32(0);
+    e.integratorCommission = BigInt.fromI32(0);
+    e.integrator = Address.empty(); 
     e.save();
 
     const vnftIntegration = vNFTIntegration.load(event.address);
@@ -50,6 +53,36 @@ export function handleSetURIPrefix(event: SetURIPrefix): void {
     const vnftIntegration = vNFTIntegration.load(event.address);
 
     vnftIntegration!.uriPrefix = event.params.uriPrefix;
+
+    vnftIntegration!.editedAt = event.block.timestamp;
+    vnftIntegration!.editedAtBlock = event.block.number;
+    vnftIntegration!.save();
+}
+
+export function handleSetOperatorCommission(event: SetOperatorCommission): void {
+    const vnftIntegration = vNFTIntegration.load(event.address);
+
+    vnftIntegration!.operatorCommission = event.params.operatorCommission;
+
+    vnftIntegration!.editedAt = event.block.timestamp;
+    vnftIntegration!.editedAtBlock = event.block.number;
+    vnftIntegration!.save();
+}
+
+export function handleSetIntegratorCommission(event: SetIntegratorCommission): void {
+    const vnftIntegration = vNFTIntegration.load(event.address);
+
+    vnftIntegration!.integratorCommission = event.params.integratorCommission;
+
+    vnftIntegration!.editedAt = event.block.timestamp;
+    vnftIntegration!.editedAtBlock = event.block.number;
+    vnftIntegration!.save();
+}
+
+export function handleSetIntegrator(event: SetIntegrator): void {
+    const vnftIntegration = vNFTIntegration.load(event.address);
+
+    vnftIntegration!.integrator = event.params.integrator;
 
     vnftIntegration!.editedAt = event.block.timestamp;
     vnftIntegration!.editedAtBlock = event.block.number;
