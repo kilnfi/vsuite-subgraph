931ea5ab4c1f3ec668238cb1e138a6703095affd
mortimr <iulian@rotaru.fr>
2023-12-14 02:49:10 +0100

    hotfix: missing import

--- src/utils/shouldSkip.ts
+++ src/utils/shouldSkip.ts
@@ -1,4 +1,4 @@
-import { ethereum } from '@graphprotocol/graph-ts';
+import { dataSource, ethereum } from '@graphprotocol/graph-ts';
 
 const skippedEvents: string[] = [
   '0x27a0ee1a915df7730fd0fe096ad743159b26b7fd0c8514f80d78f2fc079e05cf_288_mainnet'.toLowerCase(), // SetDepositedEthers
